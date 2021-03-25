ea5aeb8a401c78491ae53d7ae05eea67074217f2
John Mangel <johnmangel@Johns-MacBook-Air.local>
2021-03-25 11:13:41 -0700

    fix chord chart alignment on desktop
    remove extra wrapping div
    we are now able to remove the hacky min-width

--- frontend/src/Steps/PlayAlong.tsx
+++ frontend/src/Steps/PlayAlong.tsx
@@ -25,101 +25,96 @@ const PlayAlong: React.FC<{
 }) => {
 let copiedChordRows = chordRowObjects.slice();
   return (
-    <div
-      className="d-flex flex-grow-1 align-items-center"
+    <Row
+      className="d-flex flex-grow-1 align-items-center pr-1"
     >
-      <Row
-        className="pr-1"
-        style={{minWidth: '100%'}} // unclear why this is needed Play-Along step on desktop, it's a lazy hack
-      >
-        {
-          (isPlaying && metronomeCountIn > 0) && (
-            <Modal
-              toggle={() => pause()}
-              isOpen
-              fade={false}
-              centered
-              backdropClassName="play-along--count-in-modal--backdrop"
-              contentClassName="play-along--count-in-modal--content"
+      {
+        (isPlaying && metronomeCountIn > 0) && (
+          <Modal
+            toggle={() => pause()}
+            isOpen
+            fade={false}
+            centered
+            backdropClassName="play-along--count-in-modal--backdrop"
+            contentClassName="play-along--count-in-modal--content"
+          >
+            <ModalBody
+              className="d-flex justify-content-center border-0"
+              style={{ fontSize: '19em' }}
             >
-              <ModalBody
-                className="d-flex justify-content-center border-0"
-                style={{ fontSize: '19em' }}
-              >
-                  {metronomeCountIn}
-              </ModalBody>
-            </Modal>
-          )
-        }
+                {metronomeCountIn}
+            </ModalBody>
+          </Modal>
+        )
+      }
 
-        {
-          measureInfos.map((memoizedMeasure: MeasureInfo, index: number) => {
-            const { chordCount, beatsPerMeasure } = memoizedMeasure;
-            const measureChords = copiedChordRows.slice(0, chordCount);
-            copiedChordRows = copiedChordRows.slice(chordCount);
+      {
+        measureInfos.map((memoizedMeasure: MeasureInfo, index: number) => {
+          const { chordCount, beatsPerMeasure } = memoizedMeasure;
+          const measureChords = copiedChordRows.slice(0, chordCount);
+          copiedChordRows = copiedChordRows.slice(chordCount);
 
-            let uniformColumnSize: number | null;
-            if (measureChords.every((measureChord: ChordRowObject) => measureChord.beats === measureChords[0].beats)) uniformColumnSize = 12 / measureChords.length;
-            let totalBeatsInMeasure = 0;
-            measureChords.forEach((measureChord) => totalBeatsInMeasure += parseInt(measureChord.beats));
+          let uniformColumnSize: number | null;
+          if (measureChords.every((measureChord: ChordRowObject) => measureChord.beats === measureChords[0].beats)) uniformColumnSize = 12 / measureChords.length;
+          let totalBeatsInMeasure = 0;
+          measureChords.forEach((measureChord) => totalBeatsInMeasure += parseInt(measureChord.beats));
 
-            return (
-              <Col xs={3}>
-                <Row className="pl-1 pb-1 h-100">
-                  {measureChords.map((chordRowObject: ChordRowObject) => {
-                    const { chordNote, chordQuality, bassNote, selectedScale, selectedScaleRoot, beats } = chordRowObject;
-                    const scales = scalesForChordRowObject(chordRowObject);
+          return (
+            <Col xs={3}>
+              <Row className="pl-1 pb-1 h-100">
+                {measureChords.map((chordRowObject: ChordRowObject) => {
+                  const { chordNote, chordQuality, bassNote, selectedScale, selectedScaleRoot, beats } = chordRowObject;
+                  const scales = scalesForChordRowObject(chordRowObject);
 
-                    const selectedNamedScale = scales.find((namedScale: NamedScale) => namedScale.scaleName === selectedScale && (
-                      namedScale.scaleNotes[0] === (selectedScaleRoot || chordNote)
-                    ));
+                  const selectedNamedScale = scales.find((namedScale: NamedScale) => namedScale.scaleName === selectedScale && (
+                    namedScale.scaleNotes[0] === (selectedScaleRoot || chordNote)
+                  ));
 
-                    let borderColor = '';
-                    if (selectedNamedScale) {
-                      borderColor = scaleToHexColor(selectedNamedScale.rootScale, selectedNamedScale.rootScaleNote, monochromaticSchemes);
-                    }
+                  let borderColor = '';
+                  if (selectedNamedScale) {
+                    borderColor = scaleToHexColor(selectedNamedScale.rootScale, selectedNamedScale.rootScaleNote, monochromaticSchemes);
+                  }
 
-                    const style = borderColor ? { borderTop: `3px solid ${borderColor}` } : { borderTop: `3px solid transparent`};
+                  const style = borderColor ? { borderTop: `3px solid ${borderColor}` } : { borderTop: `3px solid transparent`};
 
-                    const activeMeasureStyle = measurePlaybackIndex === index ? {
-                      backgroundColor: 'white', color: 'black'
-                    } : {
-                      backgroundColor: 'rgb(255,255,255,0.2)'
-                    }
+                  const activeMeasureStyle = measurePlaybackIndex === index ? {
+                    backgroundColor: 'white', color: 'black'
+                  } : {
+                    backgroundColor: 'rgb(255,255,255,0.2)'
+                  }
 
-                    const parsedNumSpaces = parseInt(beats);
-                    // use uniformColumnSize if present, otherwise calculate against beatsPerMeasure
-                    // TODO: the expression `(parsedNumSpaces * (12 / beatsPerMeasure))` doesn't work as
-                    // expected because a 12-beat measure might still only have 4 chords in iReal Pro
-                    const colProps = { xs: (parsedNumSpaces * (12 / totalBeatsInMeasure)) }
+                  const parsedNumSpaces = parseInt(beats);
+                  // use uniformColumnSize if present, otherwise calculate against beatsPerMeasure
+                  // TODO: the expression `(parsedNumSpaces * (12 / beatsPerMeasure))` doesn't work as
+                  // expected because a 12-beat measure might still only have 4 chords in iReal Pro
+                  const colProps = { xs: (parsedNumSpaces * (12 / totalBeatsInMeasure)) }
 
-                    return (
-                      <Col className="play-along--chord px-0" style={{...style, ...activeMeasureStyle }} {...colProps}>
-                        <Row className="d-flex flex-row-reverse mx-0" style={{ fontSize: '1em', lineHeight: 1 }}>
-                          <div className="pr-1">
-                            {chordQuality || (<br />)}
-                          </div>
-                        </Row>
-                        <Row className="d-flex justify-content-center py-0 mx-0" style={{ fontSize: '1.5em', lineHeight: 1 }}>
-                          {/* <div> */}
-                            {chordNote}
-                          {/* </div> */}
-                        </Row>
-                        <Row className="d-flex flex-row-reverse mx-0" style={{ fontSize: '1em', lineHeight: 1 }}>
-                          <div className="pr-1">
-                            {bassNote || (<br />) }
-                          </div>
-                        </Row>
-                      </Col>
-                    );
-                  })}
-                </Row>
-              </Col>
-            );
-          })
-        }
-      </Row>
-    </div>
+                  return (
+                    <Col className="play-along--chord px-0" style={{...style, ...activeMeasureStyle }} {...colProps}>
+                      <Row className="d-flex flex-row-reverse mx-0" style={{ fontSize: '1em', lineHeight: 1 }}>
+                        <div>
+                          {chordQuality || (<br />)}
+                        </div>
+                      </Row>
+                      <Row className="d-flex justify-content-center py-0 mx-0" style={{ fontSize: '1.5em', lineHeight: 1 }}>
+                        {/* <div> */}
+                          {chordNote}
+                        {/* </div> */}
+                      </Row>
+                      <Row className="d-flex flex-row-reverse mx-0" style={{ fontSize: '1em', lineHeight: 1 }}>
+                        <div>
+                          {bassNote || (<br />) }
+                        </div>
+                      </Row>
+                    </Col>
+                  );
+                })}
+              </Row>
+            </Col>
+          );
+        })
+      }
+    </Row>
   );
 }
 
