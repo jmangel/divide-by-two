0e32991796ee79fa1294e67acdf5009d1ed38905
John Mangel <john.m5321@yahoo.com>
2021-03-31 16:50:16 -0700

    center piano visualization popover

--- frontend/src/Steps/PlayAlong.tsx
+++ frontend/src/Steps/PlayAlong.tsx
@@ -146,9 +146,9 @@ const PlayAlong: React.FC<{
                   boundariesElement={document.getElementById('play-along-grid') || 'scrollParent'}
                 >
                   <PopoverBody
-                    className="d-flex border-0 bg-light"
+                    className="d-flex border-0 bg-light justify-content-center"
                   >
-                    <Col>
+                    <div>
                       {measureChords.map((measureChord, index) => {
                         const maxHeightPercentage = 25 / measureChords.length;
                         const maxHeight = `${maxHeightPercentage}vh`;
@@ -157,17 +157,17 @@ const PlayAlong: React.FC<{
                         const defaultHeight = '50vw';
                         const defaultWidth = '100vw';
                         return (
-                          <Row className="justify-content-center"
+                          <div className="px-2"
                             style={{ width: defaultWidth, height: defaultHeight, maxHeight }}
                           >
                             <ChordPianoVisualization
                               chordRowObject={measureChord}
                               monochromaticSchemes={monochromaticSchemes}
                             />
-                          </Row>
+                          </div>
                         )
                       })}
-                    </Col>
+                    </div>
                   </PopoverBody>
                 </Popover>
               )}
