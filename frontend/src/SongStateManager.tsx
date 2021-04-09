3ca53d335942843f2f383fa30c7181111b87b170
John Mangel <john.m5321@yahoo.com>
2021-04-09 14:41:50 -0700

    extract paramConfigMap to new file

---
+++ frontend/src/SongStateManager.tsx
@@ -0,0 +1,12 @@
+import { ArrayParam, NumberParam, StringParam } from "serialize-query-params";
+
+export const paramConfigMap = {
+  a: ArrayParam,
+  c: StringParam,
+  t: StringParam,
+  i: NumberParam,
+  s: NumberParam,
+  m: StringParam,
+  k: StringParam,
+  b: NumberParam,
+};
