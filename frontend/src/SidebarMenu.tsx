dc0c60dbfe9693eb764a64754a59f4dd199db3f7
John Mangel <john.m5321@yahoo.com>
2021-04-12 09:26:38 -0700

    add Donate link to venmo

--- frontend/src/SidebarMenu.tsx
+++ frontend/src/SidebarMenu.tsx
@@ -213,6 +213,8 @@ const SidebarMenu: React.FC<{
           <BsToggleOff className="float-right" size='1.5em' />
         ) }
       </a>
+      <a id="venmo" className="menu-item" href="venmo://paycharge?txn=pay&recipients=JohnMangel&note=SongScaler!">Donate (opens in venmo)</a>
+      {/* <a id="venmo" className="menu-item" href="venmo://paycharge?txn=pay&recipients=JohnMangel&amount=5&note=SongScaler!">Buy Me A Coffee</a> */}
       <a id="feedback" className="menu-item" href="mailto:songscaler+feedback@gmail.com">Send Feedback</a>
       <Toast
         style={{ bottom: '1rem', position: 'fixed', backgroundColor: 'var(--secondary-bg-color)' }}
